5045dbac16afd0e433af5a0eedb647cb5a38a8ae
TheDorili <92449979+TheDorili@users.noreply.github.com>
2022-10-22 13:38:48 +0200

    Add files via upload
    Created a new WebApplication, works better than the old one. Changed classes and controllers. authentification = verify; data = datenverwaltung. To do: sharingcontroller

---
+++ WebApplicationTestFile/WebApplicationTestFile/Models/Data.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplicationTestFile.Models
+{
+    public class Data
+    {
+        public int storageId { get; set; }
+
+        public string? name {get; set;}  
+
+        public int date { get; set; }
+    }
+}
---
+++ WebApplicationTestFile/WebApplicationTestFile/Models/verify.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplicationTestFile.Models
+{
+    public class verify
+
+    {
+        public int Userid { get; set; } 
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
