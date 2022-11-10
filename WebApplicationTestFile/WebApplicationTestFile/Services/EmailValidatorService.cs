972fe25ab865d5d45edea9b8a15a8c97c1a3df20
TheDorili <dorian.pfeiler@me.com>
2022-11-10 13:37:34 +0100

    Made a services folder and DI

---
+++ WebApplicationTestFile/WebApplicationTestFile/Services/EmailValidatorService.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using WebApplicationTestFile.Models;
+
+namespace WebApplicationTestFile.Services
+{
+    public interface IUserIdValidatorService
+    {
+        bool ValidUser(verify request);
+    }
+    /// <summary>
+    /// //
+    /// </summary>
+    public class UserIdValidatorService : IUserIdValidatorService
+    {
+        public bool ValidUser(verify request)
+        {
+            bool validUser = false;
+        }
+        verify gefunden = new verify();
+            foreach (verify forEachVariable in LoginList)
+            {
+                if (forEachVariable.Userid==id)
+                {
+                    gefunden = forEachVariable;
+                }
+}
+
+if (gefunden.Userid == 0)
+{
+    return NotFound();
+}
+else
+{
+    return Ok(gefunden);
+}
+        }
---
+++ WebApplicationTestFile/WebApplicationTestFile/Services/PasswordValidatorService.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using WebApplicationTestFile.Models;
+
+namespace WebApplicationTestFile.Services
+{
+    public interface IPasswordValidatorService
+    {
+        bool ValidPass(verify request);
+    }
+    public class PasswordValidatorService : IPasswordValidatorService
+    {
+        public bool ValidPass(verify request)
+        {
+            bool valid = false;
+            if (request.Password != null)
+            {
+                if (request.Password == request.Password)
+                {
+                    valid = true;
+                }
+                else
+                {
+                    valid = false;
+                }
+
+            }
+            else
+            {
+                valid = false;
+            }
+            return valid;
+        }
+    
+    }
+   
+}
