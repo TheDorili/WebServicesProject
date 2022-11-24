47783d7b177cdabaff678c002c875cc91b51c236
TheDorili <dorian.pfeiler@me.com>
2022-11-24 21:46:59 +0100

    commit

--- WebApplicationTestFile/WebApplicationTestFile/Controllers/verifyController.cs
+++ WebApplicationTestFile/WebApplicationTestFile/Controllers/verifyController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebApplicationTestFile.Models;
 using WebApplicationTestFile.Services;
+using verify = WebApplicationTestFile.Services.verify;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
--- WebApplicationTestFile/WebApplicationTestFile/Services/Userlist.cs
+++ WebApplicationTestFile/WebApplicationTestFile/Services/Userlist.cs
@@ -5,8 +5,8 @@
     {
         public int Userid { get; set; }
 
-        public string Email { get; set; }
+        public string ?Email { get; set; }
 
-        public string Password { get; set; }
+        public string ?Password { get; set; }
     }
 }
