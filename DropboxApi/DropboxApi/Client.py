eddec8766ba6ced406d70bc8ade91ffa3554c6ef
TheDorili <dorian.pfeiler@me.com>
2022-11-26 10:51:29 +0100

    created a client app for "login", not working yet

---
+++ DropboxApi/DropboxApi/Client.py
@@ -0,0 +1,41 @@
+﻿import requests
+import json
+from contextlib import contextmanager
+import sys, os
+
+class client():
+	def __init__(self):
+		self.api_url = "https://localhost:"
+		self.data = {}
+		self.headers : str={
+			'Content-type' : 'application/json',
+			'Accept' : 'application/json'
+			'XApiKey' : '1234'}
+
+	def exit_client(self, api_exit):
+		if api_exit.status_code == 200:
+			return
+		sys.exit(f'\n{api_exit.json()}, Client wurde beendet\n')
+
+	def login(self):
+		api_connect = self.api_url +"/Login"
+		print(f'\nLoginsequenz wird gestartet')
+		userid = input("Bitte userid eingeben: ")
+		email = input("Bitte Email eingeben: ")
+		password = input("Bitte Passwort eingeben: ")
+
+		credentials = {
+			"Userid" : userid
+			"Email" : email
+			"Password" : Password
+		}
+
+		response = request.post(api_connect, verify = False, header = self.headers, json = bewertung)
+		self.exit_client(response)
+
+		print(response.json())
+		sys.exit("logged in")
+
+if __name__ == "__main__":
+	c = client()
+	c.login()
